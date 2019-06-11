bb2f6ae108bc4bca85f7e9ac52220a9a43200c3f
Zhouguangfa123 <390541670@qq.com>
2019-06-11 16:53:31 +0800

    用户建表语句提交

---
+++ src/main/resource/sql/sql/wc_user_info.sql
@@ -0,0 +1,34 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : zhou
+Source Server Version : 80016
+Source Host           : localhost:3306
+Source Database       : mysql
+
+Target Server Type    : MYSQL
+Target Server Version : 80016
+File Encoding         : 65001
+
+Date: 2019-06-11 16:50:00
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for wc_user_info
+-- ----------------------------
+DROP TABLE IF EXISTS `wc_user_info`;
+CREATE TABLE `wc_user_info` (
+  `user_id` int(10) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_phone` char(11) NOT NULL,
+  `user_status` char(2) NOT NULL,
+  `user_system` char(2) DEFAULT NULL,
+  `spare1` varchar(255) DEFAULT NULL,
+  `spare2` varchar(255) DEFAULT NULL,
+  `spare3` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
