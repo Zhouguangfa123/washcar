87c828dfad4f685200f8204e75986c5ba6bf4bc2
zhou <390541670@qq.com>
2019-06-17 22:58:33 +0800

    用户登录

--- src/main/java/com/hope/washcar/service/UserInfoService.java
+++ src/main/java/com/hope/washcar/service/UserInfoService.java
@@ -2,6 +2,7 @@ package com.hope.washcar.service;
 
 import com.hope.washcar.bean.UserInfoBean;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,4 +20,12 @@ public interface UserInfoService {
      * @return Map
      */
     Map<String,Object> getUserInfoList(UserInfoBean userInfoBean);
+
+    /**
+     * checkUser 判断用户是否存在
+     *
+     * @param  userInfoBean 用户bean对象
+     * @return List
+     */
+    List<UserInfoBean> checkUser(UserInfoBean userInfoBean);
 }
